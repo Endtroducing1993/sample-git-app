1e3c03d1383e7b5372012c0e2b6cc1559825d9a0
Sumit Chhabra <61081294+Endtroducing1993@users.noreply.github.com>
2023-04-14 18:44:31 +0530

    second_run

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import streamlit as st
 
 st.title('CampusX')
 
+def second_run():
+    return 'sidebar mai'
+
 col1,col2=st.columns(2)
 
 with col1:
@@ -12,3 +15,5 @@ with col2:
 st.header('Courses')
 st.subheader('Python')
 st.subheader('SQL')
+text_second=second_run()
+st.text(text_second)
